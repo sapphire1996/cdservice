66d176e3bbd72e0a45e47b11f1bf6c422aff8bd7
sapphire1996 <adepelumi1996@gmail.com>
2019-09-03 15:34:42 +0100

    notification

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,31 @@ admin.initializeApp(functions.config().firebase);
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello here Firebase!");
 });
-
+function notifyMe() {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    }
+  
+    // Let's check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      var notification = new Notification("Hi there!");
+    }
+  
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          var notification = new Notification("Hi there!");
+        }
+      });
+    }
+  
+    // At last, if the user has denied notifications, and you 
+    // want to be respectful there is no need to bother them any more.
+  }
 const createNotification=((notification) =>{
     return admin.firestore().collection('notifications')
     .add(notification)
